0df335a179a89aaff71ae6289d643a40a75fc615
Sunagimo <>
2019-03-18 01:52:32 +0900

    feat: init commit

---
+++ ECSSample/Assets/Scripts/EntityCountSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using TMPro;
+
+public class EntityCountSystem : ComponentSystem
+{
+    struct Group
+    {
+        public readonly int Length;
+        public ComponentDataArray<CountData> countData;
+    }
+
+    [Inject]
+    Group group;
+
+    protected override void OnUpdate()
+    {
+        for(var idx = 0; idx < group.Length; ++idx)
+        {
+            var countData = group.countData[idx];
+            countData.Count++;
+            group.countData[idx] = countData;
+        }
+    }
+}
